8d878e2af3ed0f2966ba69c808bc59ecc8acee89
Esteban Macanek <estebanmacanek@hotmail.com>
2022-11-07 11:19:32 -0300

    Add button

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 let container = document.querySelector('.container');
 
-for (i = 0; i < 16; i++) {
-    let line = document.createElement('div');
-    line.classList.add('line');
-    container.append(line);
-    for (u = 0; u < 16; u++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        line.appendChild(cell);
+function makeGrid (numberOfLines) {
+    for (i = 0; i < numberOfLines; i++) {
+        let line = document.createElement('div');
+        line.classList.add('line');
+        container.append(line);
+        for (u = 0; u < numberOfLines; u++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            line.appendChild(cell);
+        }
     }
 }
 
-let cells = document.querySelectorAll('.cell');
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
+function mouseover () {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
     });
+}
+
+makeGrid(16);
+mouseover();
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    numberOfLines = parseInt(prompt('How many lines?'));
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    makeGrid(numberOfLines);
+    mouseover();
 });
